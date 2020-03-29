de2ab167fc12942a49b3f9c0a503f7a7eb8b242c
Your Name <you@example.com>
2020-03-30 01:44:46 +0200

    zavrsena_validacija

--- PayMobileApp/src/helperFunctions2.js
+++ PayMobileApp/src/helperFunctions2.js
@@ -39,23 +39,46 @@ export const validateLength = (
 export const validateName = async (name, setErrors, nameType) => {
   if (!validateRequired(name, setErrors, nameType)) return false;
   if (!validateLength(name, setErrors, nameType, 4, 40)) return false;
-  setErrors(prevState => ({ ...prevState, [nameType]: null }));
-  return true;
-};
-
+  const regExpr = /^[a-zA-Z\s]*$/;
+  if (!regExpr.test(name)) {
+    setErrors(prevState => ({
+      ...prevState,
+      email: "Only letters and spaces are allowed"
+    }));
+    return false;
+    }
+    setErrors(prevState => ({ ...prevState, [nameType]: null }));
+    return true;
+    };
 export const validateCardNum = async (card, setErrors, nameType) => {
     if (!validateRequired(card, setErrors, nameType)) return false;
     if (!validateLength(card, setErrors, nameType, 16, 16)) return false;
-    setErrors(prevState => ({ ...prevState, [nameType]: null }));
-    return true;
-  };
+    const regExpr = /^[0-9]*$/;
+    if (!regExpr.test(card)) {
+      setErrors(prevState => ({
+        ...prevState,
+        email: "Only numbers are allowed"
+      }));
+      return false;
+      }
+      setErrors(prevState => ({ ...prevState, [nameType]: null }));
+      return true;
+      };
 
   export const validateCvc = async (cvc, setErrors, nameType) => {
     if (!validateRequired(cvc, setErrors, nameType)) return false;
     if (!validateLength(cvc, setErrors, nameType, 3, 3)) return false;
-    setErrors(prevState => ({ ...prevState, [nameType]: null }));
-    return true;
-  };
+    const regExpr = /^[0-9]*$/;
+    if (!regExpr.test(cvc)) {
+      setErrors(prevState => ({
+        ...prevState,
+        email: "Only numbers are allowed"
+      }));
+      return false;
+      }
+      setErrors(prevState => ({ ...prevState, [nameType]: null }));
+      return true;
+      };
 
 
 
@@ -68,7 +91,7 @@ export const validateForm = async (form, setErrors) => {
   let isValid = true;
   for (const key in form) {
     if (key === "cardNumber")
-      isValid = (await validateCardNum(form[key], setErrors)) && isValid;
+      isValid = validateCardNum(form[key], setErrors,key) && isValid;
     else if (key === "accountOwner")
       isValid = validateName(form[key], setErrors, key) && isValid;
     else if (key === "cvc")
