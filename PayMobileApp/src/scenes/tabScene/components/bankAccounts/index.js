a7510f7ac69eb7e68a28f2884866604549d18199
Dzandza <dtabakovic1@etf.unsa.ba>
2020-03-30 15:19:37 +0200

    commit zbog mergea

--- PayMobileApp/src/scenes/tabScene/components/bankAccounts/index.js
+++ PayMobileApp/src/scenes/tabScene/components/bankAccounts/index.js
@@ -1,12 +1,11 @@
-import React, { useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, ScrollView } from "react-native";
-import ConfirmDeleteModal from "./confirmDeleteModal";
 import { Accordion, List, Button, Modal } from "@ant-design/react-native";
 import styles from "./styles";
 import axios from "axios";
-import { BASE_URL } from "../../../../app/apiConfig"
+import { BASE_URL } from "../../../../app/apiConfig";
 import { useAuthContext } from "../../../../contexts/AuthContext";
-
+import { Actions } from "react-native-router-flux";
 
 const BankAccounts = () => {
   const [accounts, setAccounts] = useState([]);
@@ -14,19 +13,21 @@ const BankAccounts = () => {
   const { token } = useAuthContext();
 
   const [activeSections, setActiveSections] = useState([0]);
-  const [isDeleteModalVisible, setDeleteModalVisible] = useState(false);
-  const deleteBtn = key => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onPress = key => {
     Modal.alert("Account will be deleted. Do you want to continue?", null, [
       {
         text: "Yes",
         onPress: () =>
-          setAccounts(prevState =>
-            prevState.filter(account => account.cardNumber !== key),
+          setAccounts(
+            prevState =>
+              prevState.filter(account => account.cardNumber !== key),
             serverDelete(account.id)
           ),
-          
-        style: { color: "red" }
 
+        style: { color: "red" }
       },
       {
         text: "NO",
@@ -35,49 +36,53 @@ const BankAccounts = () => {
     ]);
   };
 
-  const serverConnection = async () => {
+  const loadAccounts = async () => {
     try {
-      
-      const { data } = await axios.get(`${BASE_URL}api/accounts/all`, { 
-        headers: {
-        authorization: `${token.tokenType} ${token.accessToken}`
-      }
-    });
-
-    console.log(data);
-
-    setAccounts(data);
-
-      console.log(help);
-     
-    } catch (error) {
-      
-      console.log("Greska" + error);
-    } 
-  };
-
-  const serverDelete = async (accountId) => {
-    //za token se radi get
-    
-    try {
-      const { data } = await axios.delete(`${BASE_URL}api/accounts/delete/`+accountId``, {
+      setError(null);
+      setLoading(true);
+      const { data } = await axios.get(`${BASE_URL}api/accounts/all`, {
         headers: {
           authorization: `${token.tokenType} ${token.accessToken}`
         }
       });
-    console.log(data);
+      data.expiryDate = new Date(
+        Number(data.expiryDate.split("-")[0]),
+        Number(data.expiryDate.split("-")[1]) - 1
+      );
+      console.log(data)
+      setAccounts(data);
     } catch (error) {
-      
-      return false;
-    } 
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
   };
-  
+
+  const serverDelete = async accountId => {
+    //za token se radi get
+
+    try {
+      const { data } = await axios.delete(
+        `${BASE_URL}api/accounts/delete/` + accountId``,
+        {
+          headers: {
+            authorization: `${token.tokenType} ${token.accessToken}`
+          }
+        }
+      );
+      console.log(data);
+    } catch (error) {
+      return false;
+    }
+  };
+
   useEffect(() => {
-    serverConnection();
-  }, []);
+    if (Actions.currentScene === "tabScene") {
+      loadAccounts();
+    }
+  }, [Actions.currentScene]);
 
   return (
-    
     <ScrollView>
       <View style={styles.header}>
         <Text style={styles.title}>My Accounts</Text>
@@ -92,7 +97,7 @@ const BankAccounts = () => {
             <Accordion.Panel key={index} header={account.accountOwner}>
               <List>
                 <List.Item style={styles.listItem}>
-                  {`Account Owner: ${account.accountOwner}`}
+                  {`Bank: ${account.bankName}`}
                 </List.Item>
                 <List.Item style={styles.listItem}>
                   {`Expiration Date: ${account.expiryDate}. ${account.expiryDate}.`}
@@ -105,7 +110,7 @@ const BankAccounts = () => {
                     style={styles.button}
                     activeStyle={{ ...styles.button, backgroundColor: "white" }}
                     onPress={() => {
-                      deleteBtn(account.cardNumber);
+                      onPress(account.cardNumber);
                     }}
                   >
                     <Text style={{ color: "red" }}>Delete</Text>
