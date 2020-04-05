4778151ec12e1cbb7a42c68ad24e9856858967c7
bakir <bsabljica1@etf.unsa.ba>
2020-04-05 19:43:18 +0200

    implementiran alert za submit dugme

--- PayMobileApp/src/scenes/tabScene/components/qrScanner/components/checkoutInfo/index.js
+++ PayMobileApp/src/scenes/tabScene/components/qrScanner/components/checkoutInfo/index.js
@@ -1,22 +1,68 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Alert } from "react-native";
 import styles from "./styles";
 import { Button, Toast, Modal } from "@ant-design/react-native";
+import { Actions } from "react-native-router-flux";
+import axios from "axios";
+import { BASE_URL } from "../../../../../../app/apiConfig";
+import { useAuthContext } from "../../../../../../contexts/AuthContext";
 
 const CheckoutInfo = ({ accountData, transactionData, onBackPressed }) => {
   const [items, setItems] = useState(transactionData.service.split(','));
-  console.log(transactionData.service.split(','));
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const { token } = useAuthContext();
 
   
   function successToast() {
   
-    Toast.success('Load success !!!', 1);
+    //Toast.success('Load success !!!', 1);
+    Alert.alert(
+      "Alert Title",
+      "My Alert Msg",
+      [
+        {
+          text: "Cancel",
+          onPress: () => {onBackPressed()},
+          style: "cancel"
+        }
+      ]
+    );
   }
+
+  
   const submitPaymentBtn = () => {
     //salji serveru dio URADITI
 
     successToast;
   
+  };
+
+
+  const testTransaction = async (bankAccountId, transactionId) => {
+    try {
+      console.log("AAAAAAAAA");
+      setError(null);
+      setLoading(true);
+      const { data } = await axios.post(`${BASE_URL}/api/payments/checkbalance`,{bankAccountId, transactionId},
+      {
+        headers: {
+          authorization: `${token.tokenType} ${token.accessToken}`,
+        },
+      });
+      console.log("REZULTAT: ");
+      console.log(data);
+      Actions.reset("tabScene");
+    } catch (error) {
+      if (error.message.includes("401")) {
+        console.log(error);
+        successToast();
+      }
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
 
@@ -48,10 +94,15 @@ const CheckoutInfo = ({ accountData, transactionData, onBackPressed }) => {
         <Text style={styles.item2}>Total amount: {transactionData.totalPrice} </Text>
 
         <Button
+        disabled={loading}
           activeStyle={{ backgroundColor: "#030852" }}
           style={styles.button}
           type="primary"
-          onPress={submitPaymentBtn}
+          onPress={async () => {
+            console.log("PODACI");
+            console.log(accountData.id + " " + transactionData.transactionId);
+            await testTransaction(accountData.bankAccountId, transactionData.transactionId);
+          }}
         >Submit payment</Button>
       </View>
     </View>
