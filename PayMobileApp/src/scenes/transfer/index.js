a6210caea5c79b3e7377124e0ab4ecc0adf3c229
Dzandza <dtabakovic1@etf.unsa.ba>
2020-05-03 23:47:39 +0200

    pickeri u dropdownu

--- PayMobileApp/src/scenes/transfer/index.js
+++ PayMobileApp/src/scenes/transfer/index.js
@@ -5,7 +5,7 @@ import {
   ScrollView,
   RefreshControl,
   TouchableOpacity,
-  Modal
+  Picker,
 } from "react-native";
 import {
   Accordion,
@@ -20,16 +20,12 @@ import axios from "axios";
 import { BASE_URL } from "../../app/apiConfig";
 import { useAuthContext } from "../../contexts/AuthContext";
 import { Actions } from "react-native-router-flux";
-import AccountChooser from "./components/accountChooser";
-
 
 const Transfer = () => {
   const [transactions, setTransactions] = useState([]);
   const [accounts, setAccounts] = useState([]);
-  const [selectedTransaction, setSelectedTransaction] = useState(null);
   const [showOptions, setShowOptions] = useState(false);
   const [accountData, setAccountData] = useState(null);
-
 
   const { token, logOut } = useAuthContext();
 
@@ -37,35 +33,38 @@ const Transfer = () => {
   const [pageNum, setPageNum] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(null);
+  const [accountsLoading, setAccountsLoading] = useState(false);
+  const [accountsError, setAccountsError] = useState(null);
   const [refreshing, setRefreshing] = useState(false);
+  const [direction, setDirection] = useState("allSends");
 
-  const [accountChooserModalVisible, setAccountChooserModalVisible] = useState(
-    false
-  );
-
-
-
-  const loadTransactions = async () => {
+  const loadTransactions = async (accountDataParam) => {
     try {
       setError(null);
       setLoading(true);
-      
-     
-      
-      const { data } = await axios.get(`${BASE_URL}api/accounts/moneyTransfer/allSends/${accountData.id}`, {
-        headers: {
-          authorization: `${token.tokenType} ${token.accessToken}`,
-        },
-      });
+      console.log(
+        `${BASE_URL}api/accounts/moneyTransfer/${direction}/${
+          accountDataParam ? accountDataParam.id : accountData.id
+        }`
+      );
+      const { data } = await axios.get(
+        `${BASE_URL}api/accounts/moneyTransfer/${direction}/${
+          accountDataParam ? accountDataParam.id : accountData.id
+        }`,
+        {
+          headers: {
+            authorization: `${token.tokenType} ${token.accessToken}`,
+          },
+        }
+      );
+      console.log(data)
       setCurrentPage(1);
-      //console.log("data: ");
-      
-      //console.log(data.transfers);
-      setPageNum(parseInt(data.transfers.length / 10) + (data.transfers.length % 10 === 0 ? 0 : 1));
-     setTransactions(data.transfers);
-   
-     
+      setPageNum(
+        parseInt(data.transfers.length / 10) +
+          (data.transfers.length % 10 === 0 ? 0 : 1)
+      );
+      setTransactions(data.transfers);
     } catch (error) {
       if (error.message.includes("401")) {
         logOut();
@@ -77,19 +76,36 @@ const Transfer = () => {
     }
   };
 
-  const hideAccountChooserModal = () => {
-    setAccountChooserModalVisible(false);
-    
-  };
-  const setChosenAccountData = (accountData) => {
-    setAccountData(accountData);
+  const loadAccounts = async () => {
+    try {
+      setAccountsError(null);
+      setAccountsLoading(true);
+      const { data } = await axios.get(`${BASE_URL}api/accounts/all`, {
+        headers: {
+          authorization: `${token.tokenType} ${token.accessToken}`,
+        },
+      });
+
+      setAccounts(data);
+      if (data.length > 0) {
+        setAccountData(data[0]);
+        await loadTransactions(data[0]);
+      }
+    } catch (error) {
+      if (error.message.includes("401")) {
+        logOut();
+        Actions.reset("userLogin");
+      }
+      setAccountsError(error);
+    } finally {
+      setAccountsLoading(false);
+    }
   };
 
   const onRefresh = async () => {
-    
     setShowOptions(false);
     setRefreshing(true);
-    await loadTransactions();
+    await loadAccounts();
     setRefreshing(false);
   };
 
@@ -135,7 +151,9 @@ const Transfer = () => {
                 }}
               >
                 <Text style={{ fontSize: 17 }}>Payment status:</Text>
-                <Text style={{ fontSize: 17 }}>{transaction.paymentStatus}</Text>
+                <Text style={{ fontSize: 17 }}>
+                  {transaction.paymentStatus}
+                </Text>
               </View>
             </List.Item>
             <List.Item style={styles.listItem}>
@@ -145,14 +163,12 @@ const Transfer = () => {
                   flexDirection: "row",
                 }}
               >
-                <Text style={{ fontSize: 17 }}>Time of transfer:</Text>
+                <Text style={{ fontSize: 17 }}>Date/Time:</Text>
                 <Text style={{ fontSize: 17 }}>
-                  {transaction.transferDateAndTime }
-                  
+                  {transaction.transferDateAndTime}
                 </Text>
               </View>
             </List.Item>
-            
           </List>
         </Accordion.Panel>
       );
@@ -162,51 +178,124 @@ const Transfer = () => {
   };
 
   useEffect(() => {
-    loadTransactions();
-  }, [Actions.currentScene]);
+    loadAccounts();
+  }, []);
 
   return (
-   
     <ScrollView
       refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       }
     >
-      <Modal
-        transparent
-        isVisible={accountChooserModalVisible}
-        onBackButtonPress={() => {
-          hideAccountChooserModal();
-        }}
-      >
-        <AccountChooser
-          
-          setVisible={() => {
-            hideAccountChooserModal();
-          }}
-          
-          onNextPressed={(accountData) => {
-            setChosenAccountData(accountData);
-            onRefresh();
-            setAccountChooserModalVisible(false);
-
-          }}
-        />
-      </Modal>
       <View>
-      
         <TouchableOpacity
           activeOpacity={0.9}
-          
+          onPress={() => setShowOptions((prevState) => !prevState)}
         >
           <View style={styles.header}>
             <Text style={styles.title}>Transfers</Text>
             <Icon name={showOptions ? "up" : "down"} color="#061178" />
           </View>
         </TouchableOpacity>
-       
+        {!showOptions ? null : accountsLoading ? null : accountsError ? (
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "#d6e4ff",
+              justifyContent: "center",
+              alignContent: "center",
+              width: "100%",
+            }}
+          >
+            <Text style={{ fontSize: 20, textAlign: "center", padding: 20 }}>
+              Error has occured while loading accounts. Please refresh and try
+              again!
+            </Text>
+          </View>
+        ) : !accounts || accounts.length === 0 ? (
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignContent: "center",
+              paddingVertical: 20,
+              width: "100%",
+              backgroundColor: "#d6e4ff",
+            }}
+          >
+            <Text style={{ paddingTop: 10, fontSize: 20, textAlign: "center" }}>
+              You don't have any registered accounts
+            </Text>
+          </View>
+        ) : (
+          <View
+            style={{
+              backgroundColor: "#d6e4ff",
+            }}
+          >
+            <View style={{ margin: 10, backgroundColor: "#f0f5ff" }}>
+              <Text
+                style={{
+                  padding: 10,
+                  fontSize: 17,
+                  backgroundColor: "#d6e4ff",
+                }}
+              >
+                Select your account
+              </Text>
+              <Picker
+                onValueChange={async (value) => {
+                  setAccountData(
+                    accounts.find((account) => account.cardNumber === value)
+                  );
+                  await loadTransactions();
+                }}
+                selectedValue={accountData.cardNumber}
+              >
+                {accounts.map((account, index) => (
+                  <Picker.Item
+                    label={`${account.cardNumber}`}
+                    value={account.cardNumber}
+                    key={index}
+                  />
+                ))}
+              </Picker>
+
+              <Text
+                style={{
+                  paddingTop: 30,
+                  padding: 10,
+                  fontSize: 17,
+                  backgroundColor: "#d6e4ff",
+                }}
+              >
+                Select payment direction
+              </Text>
+              <Picker
+                style={styles.listItem}
+                onValueChange={async (value) => {
+                  setDirection(value);
+                  await loadTransactions();
+                }}
+                selectedValue={direction}
+              >
+                {[
+                  { value: "Payments", key: "allSends" },
+                  { value: "Receivements", key: "allReceives" },
+                ].map((item, index) => (
+                  <Picker.Item
+                    label={item.value}
+                    value={item.key}
+                    key={index}
+                  />
+                ))}
+              </Picker>
+            </View>
+          </View>
+        )}
       </View>
-        {loading ? (
+
+      {loading || accountsLoading ? (
         <View
           style={{
             flex: 1,
@@ -217,7 +306,7 @@ const Transfer = () => {
         >
           <ActivityIndicator size="large" color="#061178" />
         </View>
-      ) : error ? (
+      ) : error || accountsError ? (
         <View
           style={{ flex: 1, justifyContent: "center", alignContent: "center" }}
         >
