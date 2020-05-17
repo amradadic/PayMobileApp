a633630084d657874a2f7cc168bc827ad9f09a5b
adadic1 <44211420+adadic1@users.noreply.github.com>
2020-05-18 01:27:30 +0200

    pokusaj testova

--- PayMobileApp/test/test.js
+++ PayMobileApp/test/test.js
@@ -1,10 +1,11 @@
-import UserLogin from "../src/scenes/userLogin";
 import renderer from "react-test-renderer";
+import React from 'react';
+import 'react-native';
 
 
 
 test("initial test", () => {
-   // let data = renderer.create(<UserLogin/>).getInstance();
+    /*let data = renderer.create(<UserLogin/>).getInstance();*/
     let num=20;
     expect(num).toEqual(20);
   });
