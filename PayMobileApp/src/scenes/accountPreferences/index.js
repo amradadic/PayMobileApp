4fb783ea2e9644b73aca2609c5dc4ed8eaa0858c
Dzandza <dtabakovic1@etf.unsa.ba>
2020-05-18 01:49:10 +0200

    fiks

--- PayMobileApp/src/scenes/accountPreferences/index.js
+++ PayMobileApp/src/scenes/accountPreferences/index.js
@@ -13,12 +13,12 @@ import {
   Icon,
   InputItem,
   Toast,
-  List
+  List,
 } from "@ant-design/react-native";
 import {
   validateRequired,
   validateLength,
-  validateNumber
+  validateNumber,
 } from "../../helperFunctions2";
 import styles from "./styles";
 import axios from "axios";
@@ -38,23 +38,26 @@ const AccountPreferences = () => {
   const [errors, setErrors] = useState({
     balanceLowerLimit: null,
     monthlyLimit: null,
-    transactionAmountLimit: null
+    transactionAmountLimit: null,
   });
 
-  const [isCheckIconsVisible, setCheckIconsVisible] = useState(
-    {
-      balanceLowerLimitCheckIcon: null,
-      monthlyLimitCheckIcon: null,
-      transactionAmountLimitCheckIcon: null
-    }
-  );
+  const [isCheckIconsVisible, setCheckIconsVisible] = useState({
+    balanceLowerLimitCheckIcon: null,
+    monthlyLimitCheckIcon: null,
+    transactionAmountLimitCheckIcon: null,
+  });
 
-  const [isBalanceLowerLimitCheckIcon, setIsBalanceLowerLimitCheckIcon] = useState(false);
+  const [
+    isBalanceLowerLimitCheckIcon,
+    setIsBalanceLowerLimitCheckIcon,
+  ] = useState(false);
   const [isMonthlyLimitCheckIcon, setIsMonthlyLimitCheckIcon] = useState(false);
-  const [isTransactionAmountLimitCheckIcon, setIsTransactionAmountLimitCheckIcon] = useState(false);
+  const [
+    isTransactionAmountLimitCheckIcon,
+    setIsTransactionAmountLimitCheckIcon,
+  ] = useState(false);
 
   const loadAccounts = async () => {
-    console.log("loadAccounts");
     try {
       setError(null);
       setLoading(true);
@@ -65,35 +68,27 @@ const AccountPreferences = () => {
         },
       });
       setAccounts(data);
-      if (data.length > 0)
-        setAccountData(data[0]);
-    }
-    catch (error) {
+      if (data.length > 0) setAccountData(data[0]);
+    } catch (error) {
       if (error.message.includes("401")) {
         logOut();
         Actions.reset("userLogin");
       }
       setError(error);
-    }
-    finally {
-      console.log("kraj loadAccounts");
+    } finally {
       setLoading(false);
     }
   };
 
   const onRefresh = async () => {
-    console.log("onRefresh");
     setShowOptions(false);
     setRefreshing(true);
     await loadAccounts();
     setRefreshing(false);
-    console.log("kraj onRefresh");
   };
 
   const sendNewData = async (accountData) => {
-    console.log("sendNewData");
     try {
-      console.log(accountData);
       const { data } = await axios.post(
         `${BASE_URL}api/accounts/update/${accountData.id}`,
         {
@@ -107,52 +102,46 @@ const AccountPreferences = () => {
           },
         }
       );
-      if (!data.success)
-        Toast.fail(data.text, 1);
+      if (!data.success) Toast.fail(data.text, 1);
       else {
         Toast.success("Successfully updated account preferences!", 0.7);
         setTimeout(() => Actions.pop(), 700);
       }
-    }
-    catch (error) {
+    } catch (error) {
       if (error.message.includes("404"))
         Toast.fail("Bank account does not exist", 1);
       else if (error.message.includes("401")) {
         logOut();
         Actions.reset("userLogin");
-      }
-      else {
+      } else {
         console.log(error); //400 baci
         Toast.fail(
           "Failed to update account preferences. Check your inputs and try again",
           1
         );
       }
-    }
-    finally {
-      console.log("kraj sendNewData");
+    } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    console.log("useEffect");
     loadAccounts();
-    console.log("kraj useEffect");
   }, []);
 
   function showCheckIcon(trueFalseValue) {
     if (trueFalseValue != null && trueFalseValue == true) {
       const keyString = "someKey";
-      return <View style={styles.rowMemberIcon} key={keyString}>
-        <Icon
-          name="check-circle"
-          color="#40EF6D"
-          size="sm"
-          onPress={() =>
-            Toast.success("The input is in correct format")
-          } />
-      </View>
+      return (
+        <View style={styles.rowMemberIcon} key={keyString}>
+          <Icon
+            name="check-circle"
+            color="#40EF6D"
+            size="sm"
+            onPress={() => Toast.success("The input is in correct format")}
+          />
+        </View>
+      );
     }
   }
 
@@ -204,211 +193,220 @@ const AccountPreferences = () => {
             </Text>
           </View>
         ) : (
-              <View
+          <View
+            style={{
+              backgroundColor: "#d6e4ff",
+            }}
+          >
+            <View style={{ margin: 10, backgroundColor: "#f0f5ff" }}>
+              <Text
                 style={{
+                  padding: 10,
+                  fontSize: 17,
                   backgroundColor: "#d6e4ff",
                 }}
               >
-                <View style={{ margin: 10, backgroundColor: "#f0f5ff" }}>
-                  <Text
-                    style={{
-                      padding: 10,
-                      fontSize: 17,
-                      backgroundColor: "#d6e4ff",
-                    }}
-                  >
-                    Select your account
+                Select your account
               </Text>
-                  <Picker
-                    onValueChange={async (value) => {
-                      setAccountData(
-                        accounts.find((account) => account.cardNumber === value)
-                      );
-                    }}
-                    selectedValue={accountData.cardNumber}
-                  >
-                    {accounts.map((account, index) => (
-                      <Picker.Item
-                        label={`${account.cardNumber}`}
-                        value={account.cardNumber}
-                        key={index}
-                      />
-                    ))}
-                  </Picker>
-                </View>
-              </View>
-            )}
-
-        {loading ||
-          !accountData ||
-          !accountData.monthlyLimit ||
-          !accountData.balanceLowerLimit ||
-          !accountData.transactionAmountLimit ? (
-            <View
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignContent: "center",
-                paddingTop: 60,
-              }}
-            >
-              <ActivityIndicator size="large" color="#061178" />
-
+              <Picker
+                onValueChange={async (value) => {
+                  setAccountData(
+                    accounts.find((account) => account.cardNumber === value)
+                  );
+                }}
+                selectedValue={accountData.cardNumber}
+              >
+                {accounts.map((account, index) => (
+                  <Picker.Item
+                    label={`${account.cardNumber}`}
+                    value={account.cardNumber}
+                    key={index}
+                  />
+                ))}
+              </Picker>
             </View>
-          ) : error ? (
-            <View
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignContent: "center",
-              }}
-            >
-              <Text style={{ fontSize: 20, textAlign: "center", padding: 30 }}>
-                Error has occured while loading. Please refresh and try again!
+          </View>
+        )}
+
+        {loading || !accountData ? (
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignContent: "center",
+              paddingTop: 60,
+            }}
+          >
+            <ActivityIndicator size="large" color="#061178" />
+          </View>
+        ) : error ? (
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignContent: "center",
+            }}
+          >
+            <Text style={{ fontSize: 20, textAlign: "center", padding: 30 }}>
+              Error has occured while loading. Please refresh and try again!
             </Text>
-            </View>
-          ) : (
-              <>
-                <Text style={styles.text}>Balance lower limit:</Text>
-                <View style={styles.row}>
-                  <View style={styles.rowMemberInput}>
-
-                    <InputItem
-                      placeholder="Positive number"
-                      type="number"
-                      style={styles.listItem}
-
-                      error={errors.balanceLowerLimit}
-                      onErrorClick={() =>
-                        Toast.fail(
-                          errors.balanceLowerLimit,
-                          0.05 * errors.balanceLowerLimit.length
-                        )
-                      }
-                      value={accountData.balanceLowerLimit.toString()}
-                      onChange={(value) => {
-                        if (value == "")
-                          value = "0";
-
-                        if (validateNumber(value, setErrors, setCheckIconsVisible, "balanceLowerLimit") == true)
-                          setIsBalanceLowerLimitCheckIcon(true);
-                        else
-                          setIsBalanceLowerLimitCheckIcon(false);
-
-                        setAccountData((prevState) => ({
-                          ...prevState,
-                          balanceLowerLimit: value
-                        }));
-                      }}
-                    />
-                  </View>
-                  {
-                    showCheckIcon(isBalanceLowerLimitCheckIcon)
+          </View>
+        ) : (
+          <>
+            <Text style={styles.text}>Balance lower limit:</Text>
+            <View style={styles.row}>
+              <View style={styles.rowMemberInput}>
+                <InputItem
+                  placeholder="Positive number"
+                  type="number"
+                  style={styles.listItem}
+                  error={errors.balanceLowerLimit}
+                  onErrorClick={() =>
+                    Toast.fail(
+                      errors.balanceLowerLimit,
+                      0.05 * errors.balanceLowerLimit.length
+                    )
                   }
-                </View>
+                  value={accountData.balanceLowerLimit.toString()}
+                  onChange={(value) => {
+                    if (value == "") value = "0";
 
-                <Text style={styles.text}> Monthly limit:</Text>
-                <View style={styles.row}>
-                  <View style={styles.rowMemberInput}>
+                    if (
+                      validateNumber(
+                        value,
+                        setErrors,
+                        setCheckIconsVisible,
+                        "balanceLowerLimit"
+                      ) == true
+                    )
+                      setIsBalanceLowerLimitCheckIcon(true);
+                    else setIsBalanceLowerLimitCheckIcon(false);
 
-                    <InputItem
-                      placeholder="Positive number"
-                      type="number"
-                      style={styles.listItem}
-
-                      value={accountData.monthlyLimit.toString()}
-
-                      error={errors.monthlyLimit}
-                      onErrorClick={() =>
-                        Toast.fail(
-                          errors.monthlyLimit,
-                          0.05 * errors.monthlyLimit.length
-                        )
-                      }
-                      onChange={(value) => {
-                        if (value == "")
-                          value = "0";
-
-                        if (validateNumber(value, setErrors, setCheckIconsVisible, "monthlyLimit") == true)
-                          setIsMonthlyLimitCheckIcon(true);
-                        else
-                          setIsMonthlyLimitCheckIcon(false);
-
-                        setAccountData((prevState) => ({
-                          ...prevState,
-                          monthlyLimit: value
-                        }));
-                      }}
-                    />
-                  </View>
-                  {
-                    showCheckIcon(isMonthlyLimitCheckIcon)
-                  }
-                </View>
-
-                <Text style={styles.text}>Transaction amount limit:</Text>
-                <View style={styles.row}>
-                  <View style={styles.rowMemberInput}>
-                    <InputItem
-                      placeholder="Positive number"
-                      type="number"
-                      style={styles.listItem}
-
-                      value={accountData.transactionAmountLimit.toString()}
-
-                      error={errors.transactionAmountLimit}
-                      onErrorClick={() =>
-                        Toast.fail(
-                          errors.transactionAmountLimit,
-                          0.05 * errors.transactionAmountLimit.length
-                        )
-                      }
-
-                      onChange={(value) => {
-                        if (value == "")
-                          value = "0";
-
-                        if (validateNumber(value, setErrors, setCheckIconsVisible, "transactionAmountLimit") == true)
-                          setIsTransactionAmountLimitCheckIcon(true);
-                        else
-                          setIsTransactionAmountLimitCheckIcon(false);
-
-                        setAccountData((prevState) => ({
-                          ...prevState,
-                          transactionAmountLimit: value
-                        }));
-                      }}
-                    />
-                  </View>
-                  {
-                    showCheckIcon(isTransactionAmountLimitCheckIcon)
-                  }
-                </View>
-
-                <Button
-                  activeStyle={{ backgroundColor: "#030852" }}
-                  style={styles.button}
-                  type="primary"
-                  loading={loading}
-                  disabled={loading}
-                  style={styles.button}
-                  onPress={async () => {
-
-                    if (validateNumber(accountData.transactionAmountLimit.toString(), setErrors, setCheckIconsVisible, "transactionAmountLimit") == true &&
-                      validateNumber(accountData.monthlyLimit.toString(), setErrors, setCheckIconsVisible, "monthlyLimit") == true &&
-                      validateNumber(accountData.balanceLowerLimit.toString(), setErrors, setCheckIconsVisible, "balanceLowerLimit") == true) {
-                      setLoading(true);
-                      await sendNewData(accountData);
-                    }
-                    else
-                      setLoading(false);
+                    setAccountData((prevState) => ({
+                      ...prevState,
+                      balanceLowerLimit: value,
+                    }));
                   }}
+                />
+              </View>
+              {showCheckIcon(isBalanceLowerLimitCheckIcon)}
+            </View>
 
-                >
-                  SAVE CHANGES
+            <Text style={styles.text}> Monthly limit:</Text>
+            <View style={styles.row}>
+              <View style={styles.rowMemberInput}>
+                <InputItem
+                  placeholder="Positive number"
+                  type="number"
+                  style={styles.listItem}
+                  value={accountData.monthlyLimit.toString()}
+                  error={errors.monthlyLimit}
+                  onErrorClick={() =>
+                    Toast.fail(
+                      errors.monthlyLimit,
+                      0.05 * errors.monthlyLimit.length
+                    )
+                  }
+                  onChange={(value) => {
+                    if (value == "") value = "0";
+
+                    if (
+                      validateNumber(
+                        value,
+                        setErrors,
+                        setCheckIconsVisible,
+                        "monthlyLimit"
+                      ) == true
+                    )
+                      setIsMonthlyLimitCheckIcon(true);
+                    else setIsMonthlyLimitCheckIcon(false);
+
+                    setAccountData((prevState) => ({
+                      ...prevState,
+                      monthlyLimit: value,
+                    }));
+                  }}
+                />
+              </View>
+              {showCheckIcon(isMonthlyLimitCheckIcon)}
+            </View>
+
+            <Text style={styles.text}>Transaction amount limit:</Text>
+            <View style={styles.row}>
+              <View style={styles.rowMemberInput}>
+                <InputItem
+                  placeholder="Positive number"
+                  type="number"
+                  style={styles.listItem}
+                  value={accountData.transactionAmountLimit.toString()}
+                  error={errors.transactionAmountLimit}
+                  onErrorClick={() =>
+                    Toast.fail(
+                      errors.transactionAmountLimit,
+                      0.05 * errors.transactionAmountLimit.length
+                    )
+                  }
+                  onChange={(value) => {
+                    if (value == "") value = "0";
+
+                    if (
+                      validateNumber(
+                        value,
+                        setErrors,
+                        setCheckIconsVisible,
+                        "transactionAmountLimit"
+                      ) == true
+                    )
+                      setIsTransactionAmountLimitCheckIcon(true);
+                    else setIsTransactionAmountLimitCheckIcon(false);
+
+                    setAccountData((prevState) => ({
+                      ...prevState,
+                      transactionAmountLimit: value,
+                    }));
+                  }}
+                />
+              </View>
+              {showCheckIcon(isTransactionAmountLimitCheckIcon)}
+            </View>
+
+            <Button
+              activeStyle={{ backgroundColor: "#030852" }}
+              style={styles.button}
+              type="primary"
+              loading={loading}
+              disabled={loading}
+              style={styles.button}
+              onPress={async () => {
+                if (
+                  validateNumber(
+                    accountData.transactionAmountLimit.toString(),
+                    setErrors,
+                    setCheckIconsVisible,
+                    "transactionAmountLimit"
+                  ) == true &&
+                  validateNumber(
+                    accountData.monthlyLimit.toString(),
+                    setErrors,
+                    setCheckIconsVisible,
+                    "monthlyLimit"
+                  ) == true &&
+                  validateNumber(
+                    accountData.balanceLowerLimit.toString(),
+                    setErrors,
+                    setCheckIconsVisible,
+                    "balanceLowerLimit"
+                  ) == true
+                ) {
+                  setLoading(true);
+                  await sendNewData(accountData);
+                } else setLoading(false);
+              }}
+            >
+              SAVE CHANGES
             </Button>
-              </>
-            )}
+          </>
+        )}
       </View>
     </ScrollView>
   );
